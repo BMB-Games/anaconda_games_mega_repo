e3c9a6015bdaab05fc97156b81fd49b65a748eca
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2025-05-13 22:21:19 +0200

    FEAT Keyboard inputs

--- brick_breaker/window.py
+++ brick_breaker/window.py
@@ -9,14 +9,25 @@ pygame.display.set_caption("Test")
 screen.fill((0, 0, 0))
 
 ball = pygame.image.load("./images/football.png")
+ball = pygame.transform.scale(ball, (24, 24))
+
+ball_y = HEIGHT//2 - ball.get_height()//2
+ball_x = WIDTH//2 - ball.get_width()//2
 
 game_over = False
 
 while not game_over:
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
-    screen.blit(ball, (WIDTH//2 - ball.get_width()//2, HEIGHT//2 - ball.get_height()//2))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT and ball_x + ball.get_width() < WIDTH:
+                ball_x += 10
+            elif event.key == pygame.K_LEFT and ball_x > 0:
+                ball_x -= 10
+
+    screen.blit(ball, (ball_x, ball_y))
     pygame.display.update()
 
 
