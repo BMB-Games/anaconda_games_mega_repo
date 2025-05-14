233584b97f71892fb451a232dcccb8ff197f3485
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2025-05-14 12:35:13 +0200

    Enhance ball movement and cleanup game loop logic
    Replaced keydown events with continuous key press detection for smoother ball movement. Added delta time for consistent movement speed and refined game logic. Minor cleanup includes removing redundant lines and standardizing spacing.

--- brick_breaker/window.py
+++ brick_breaker/window.py
@@ -7,7 +7,6 @@ HEIGHT = 480
 base_path = os.path.dirname(os.path.abspath(__file__))
 imag_path = os.path.join(base_path, "images", "football.png")
 
-
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 pygame.display.set_caption("Test")
@@ -16,24 +15,33 @@ screen.fill((0, 0, 0))
 ball = pygame.image.load(imag_path)
 ball = pygame.transform.scale(ball, (24, 24))
 
-ball_y = HEIGHT//2 - ball.get_height()//2
-ball_x = WIDTH//2 - ball.get_width()//2
+ball_y = HEIGHT // 2 - ball.get_height() // 2
+ball_x = WIDTH // 2 - ball.get_width() // 2
 
 game_over = False
 
+clock = pygame.time.Clock()
+
 while not game_over:
+    dt = clock.tick(60)
     screen.fill((0, 0, 0))
+    pressed = pygame.key.get_pressed()
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:
+        if (event.type == pygame.QUIT
+                or pressed[pygame.K_ESCAPE]):
             game_over = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT and ball_x + ball.get_width() < WIDTH:
-                ball_x += 10
-            elif event.key == pygame.K_LEFT and ball_x > 0:
-                ball_x -= 10
+    if pressed[pygame.K_RIGHT] and ball_x + ball.get_width() < WIDTH:
+        ball_x += 0.8 * dt
+    if pressed[pygame.K_LEFT] and ball_x > ball.get_width() / 2:
+        ball_x -= 0.8 * dt
+    if pressed[pygame.K_UP] and ball_y > ball.get_height() / 2:
+        ball_y -= 0.8 * dt
+    if pressed[pygame.K_DOWN] and ball_y + ball.get_height() < HEIGHT:
+        ball_y += 0.8 * dt
+    if pressed[pygame.K_SPACE]:
+        ball_x, ball_y = 0, 0
 
     screen.blit(ball, (ball_x, ball_y))
     pygame.display.update()
-
 
 pygame.quit()
